cd6bb91000302a7e7e57e17c216edd76d600aa8f
Juan Eudes <jem.lopez.19@gmail.com>
2023-09-25 18:25:21 -0500

    Se agregó la clase clienteMovil

--- src/Movil/ClienteMovil.java
+++ src/Movil/ClienteMovil.java
@@ -2,7 +2,6 @@ package com.miempresa.basico;
 
 public class clienteMovil {
 
-	public static void main(String[] args) {
-		System.out.println("Hola soy un nuevo cliente");
-	}
+	public String nombre;
+	public ind id;
 }
