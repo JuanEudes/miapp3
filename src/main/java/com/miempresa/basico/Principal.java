05dc70e8f6c1fe8da406f4bd0e0b8c6649e40da5
Juan Eudes <jem.lopez.19@gmail.com>
2023-09-25 16:54:37 -0500

    tercer Commit

--- src/main/java/com/miempresa/basico/Principal.java
+++ src/main/java/com/miempresa/basico/Principal.java
@@ -3,6 +3,6 @@ package com.miempresa.basico;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola a Todos");
+		System.out.println("Hola Mundillo");
 	}
 }
