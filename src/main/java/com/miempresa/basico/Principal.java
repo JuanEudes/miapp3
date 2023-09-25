ca428c71bf16003811ad282f4d6c6c9770dd3fc7
Juan Eudes <jem.lopez.19@gmail.com>
2023-09-25 18:34:25 -0500

    Se agregó un nuevo mensaje en la clase principal

--- src/main/java/com/miempresa/basico/Principal.java
+++ src/main/java/com/miempresa/basico/Principal.java
@@ -3,6 +3,7 @@ package com.miempresa.basico;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundillo");
+		System.out.println("Hola Mundote");
+		System.out.println("Buenas noches es hora de dormir");
 	}
 }
